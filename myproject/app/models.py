61b930daeacd3649d85c818b3a5ed67d521942bf
bryanal007 <alexanderreal00@gmail.com>
2024-11-06 11:42:40 -0500

    xd

---
+++ myproject/app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Producto(models.Model):
+    nombre= models.CharField(max_length=100, blank=True)
+    descripcion = models.TextField(max_length=2000, blank=True)
+    precio = models.DecimalField(max_digits=4,decimal_places=2)
+    fecha_ingreso = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.nombre
